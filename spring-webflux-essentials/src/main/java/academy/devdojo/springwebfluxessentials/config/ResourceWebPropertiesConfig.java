3da1c0583a14cbdfe309175c496878a3676d09de
Ricardo Gomes <ricardogomes.swe@gmail.com>
2023-01-31 19:23:38 +0000

    add inMem auth

--- spring-webflux-essentials/src/main/java/academy/devdojo/springwebfluxessentials/config/ResourceWebPropertiesConfig.java
+++ spring-webflux-essentials/src/main/java/academy/devdojo/springwebfluxessentials/config/ResourceWebPropertiesConfig.java
@@ -3,6 +3,7 @@ package academy.devdojo.springwebfluxessentials.config;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.blockhound.BlockHound;
 
 @Configuration
 public class ResourceWebPropertiesConfig {
--- spring-webflux-essentials/src/main/java/academy/devdojo/springwebfluxessentials/config/SecurityConfig.java
+++ spring-webflux-essentials/src/main/java/academy/devdojo/springwebfluxessentials/config/SecurityConfig.java
@@ -1,2 +1,50 @@
-package academy.devdojo.springwebfluxessentials.config;public class SecurityConfig {
+package academy.devdojo.springwebfluxessentials.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        return http
+                .csrf().disable()
+                .authorizeExchange()
+                    .pathMatchers(HttpMethod.POST, "/shows/*").hasRole("ADMIN")
+                    .pathMatchers(HttpMethod.GET, "/shows/*").hasRole("USER")
+                .anyExchange().authenticated()
+                .and()
+                    .formLogin()
+                .and()
+                    .httpBasic()
+                .and()
+                    .build();
+
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService(){
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("user")
+                .password(encoder.encode("user"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN", "USER")
+                .build();
+        return new MapReactiveUserDetailsService(user, admin);
+    }
+
 }
